78a8e8d0d25c47c0290935e30caf478af277f253
ANSAB UDDIN <ansabuddin0346@gmail.com>
2022-08-14 02:33:04 -0700

    error page add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Route,
 } from "react-router-dom";
 import About from './Componets/About';
+import Error from './Componets/Error';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
     
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="*" element={<Error />} />
      
     </Routes>
     
---
+++ src/Componets/Error.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='container text-center my-5'>
+<h1>
+        Error 404 
+        page not found
+
+    </h1>
+    </div>
+  )
+}
+
+export default Error
